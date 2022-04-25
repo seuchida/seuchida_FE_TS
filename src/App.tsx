d849c226674cecca5253f29aa271d69ab0a33913
hoontail <sper456@naver.com>
2022-04-26 03:33:17 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Login from "Login";
 function App(): JSX.Element {
   return (
     <>
-    <div>안녕하세요~</div>
+    <div>안녕하세요~zz</div>
     <Login name = 'hello'></Login>
       {/* <Switch> */}
         {/* <Route exact path="/helloReact" component={} /> */}
